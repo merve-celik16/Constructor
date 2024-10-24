b8f5c77b1e2a7b7a60751ba1b5d7d9f463401c9a
merve-celik16 <mervedursunn34@gmail.com>
2024-10-24 11:05:08 +0300

    Proje dosyası ekle.

---
+++ Bebek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Bebek
+    {
+        //properties
+        public string Ad { get; set; }
+        public string Soyadi { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+   
+        public Bebek()      //Parametre almayan constructor
+        {
+            DogumTarihi = DateTime.Now; // Şuan ki tarihi ve zamanı getirir.
+            Console.WriteLine("Ingaaaaaaa !!! ");
+        }
+        public Bebek(string _ad,string _soyadi) // parametre alan constructor
+        {
+            Ad= _ad;
+            Soyadi= _soyadi;
+            DogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaaaaa !! ");
+
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parametresiz constructor ile nesne oluşturma
+            Bebek bebek1 = new Bebek();
+
+            // Parametreli constructor ile nesne oluşturma
+            Bebek bebek2 = new Bebek("Kerem", "ATAY");
+
+            // Bebeklerin bilgilerini yazdırma
+            Console.WriteLine($"Bebek 1:");
+            Console.WriteLine($"Adı: {bebek1.Ad}");
+            Console.WriteLine($"Soyadı: {bebek1.Soyadi}");
+            Console.WriteLine($"Doğum Tarihi: {bebek1.DogumTarihi}");
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Bebek 2:");
+            Console.WriteLine($"Adı: {bebek2.Ad}");
+            Console.WriteLine($"Soyadı: {bebek2.Soyadi}");
+            Console.WriteLine($"Doğum Tarihi: {bebek2.DogumTarihi}");
+        }
+    }
+}
